5c0f5db049eb3231d3ceacdd823191aea5489b62
Roderic Linguri <linguri@digices.com>
2016-07-19 23:19:08 -0600

    Create abstract table class.
    Extend this class to inherit insert and fetch methods.

---
+++ lib/table.php
@@ -0,0 +1,36 @@
+<?php
+
+abstract class Table
+{
+
+    protected $dbo;
+    
+    protected $tableName;
+    
+    public function fetchAllRows() {
+        $sql = 'SELECT * FROM  `'.$this->tableName.'`;';
+        if ($rows = $this->dbo->fetchRows($sql)) {
+            return $rows;
+        } else {
+            return false;
+        }
+    }
+
+    public function fetchRowFromId($id) {
+        $sql = 'SELECT * FROM  `'.$this->tableName.'` WHERE `id` = '.$id.';';
+        if ($rows = $this->dbo->fetchRows($sql)) {
+            return $rows[0];
+        } else {
+            return false;
+        }
+    }
+    
+    public function insertArray($arr) {
+        $sql = 'INSERT INTO  `'.$this->tableName.'` VALUES (NULL';
+        foreach ($arr as $val) {
+            $sql .= ",'".$val."'";
+        }
+        return $this->dbo->lastInsertId();
+    }
+
+}
