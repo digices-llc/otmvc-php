ff626f9845a7c70ca88a1e7611d9108634935bf5
Roderic Linguri <linguri@digices.com>
2016-07-20 19:27:05 -0600

    Add missing execute statement.

--- lib/table.php
+++ lib/table.php
@@ -30,6 +30,7 @@ abstract class Table
         foreach ($arr as $val) {
             $sql .= ",'".$val."'";
         }
+        $this->dbo->executeSQL($sql);
         return $this->dbo->lastInsertId();
     }
 
